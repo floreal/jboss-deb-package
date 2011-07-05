576ece34821b35c5c92c38dc50657cb05beda60d
Floréal TOUMIKIAN <ftoumikian@april.org>
2011-07-05 14:25:01 +0200

    Fixed wrong jboss destination

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ die() {
 
 rm -Rf $here/src/var/lib/jboss
 sudo rm -Rf $name
-mkdir -p $here/src/var/log/jboss src/var/lib/jboss $here/tmp
+mkdir -p $here/src/var/log/jboss $here/tmp
 if [ ! -f $archive ]; then
 	echo -n "Downloading JBoss Application Server 6.0.0 archive ... "
 	wget http://freefr.dl.sourceforge.net/project/jboss/JBoss/JBoss-6.0.0.Final/jboss-as-distribution-6.0.0.Final.zip -O $archive > /dev/null 2> /dev/null || die "failed"
@@ -21,8 +21,8 @@ unzip $archive -d $here/src/var/lib > /dev/null 2> /dev/null || die "failed"
 echo "done"
 mv $here/src/var/lib/jboss-6.0.0.Final $here/src/var/lib/jboss
 echo -n "Tainting configuration files ... "
-find $here/src/var/lib/jboss -type f -exec sed -i 's/8080/${jboss.http}/g' {} \;
-find $here/src/var/lib/jboss -type f -exec sed -i 's/8443/${jboss.https}/g' {} \;
+find $here/src/var/lib/jboss/server/*/conf -type f -exec sed -i 's/8080/${jboss.http}/g' {} \;
+find $here/src/var/lib/jboss/server/*/conf -type f -exec sed -i 's/8443/${jboss.https}/g' {} \;
 echo "done"
 sudo cp -R $here/src $here/$name
 echo -n "Packaging ... "
